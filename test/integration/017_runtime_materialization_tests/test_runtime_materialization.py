1ce3e004bde4a22103ced3b2a6aecb7914ad91e5
Jeremy Cohen <jeremy@dbtlabs.com>
2022-05-09 09:17:58 +0200

    Clean up existing_relation, load_relation,  etc

--- test/integration/017_runtime_materialization_tests/test_runtime_materialization.py
+++ test/integration/017_runtime_materialization_tests/test_runtime_materialization.py
@@ -70,6 +70,15 @@ class TestRuntimeMaterialization(DBTIntegrationTest):
         self.assertTableDoesNotExist('view__dbt_backup')
         self.assertTablesEqual("seed", "view")
 
+        # Again, but against the incremental materialization
+        self.run_dbt(['run', '--model', 'incremental'])
+        self.run_sql_file("create_incremental__dbt_tmp.sql")
+        import ipdb; ipdb.set_trace()
+        results = self.run_dbt(['run', '--model', 'incremental', '--full-refresh'])
+        self.assertEqual(len(results), 1)
+
+        self.assertTableDoesNotExist('incremental__dbt_tmp')
+        self.assertTablesEqual("seed", "incremental")
 
 class TestRuntimeMaterializationWithConfig(TestRuntimeMaterialization):
     @property
